0664aa78090641526543c0e54bb69d30583386c6
mattyocode <matthewoliver@live.co.uk>
2021-01-22 11:06:00 +0000

    First commit

---
+++ src/excel_ngrams/console.py
@@ -0,0 +1,9 @@
+import click
+
+from . import __version__
+
+@click.command()
+@click.version_option(version=__version__)
+def main():
+    """Excel n-grams project."""
+    click.echo("Excel n-grams.")
